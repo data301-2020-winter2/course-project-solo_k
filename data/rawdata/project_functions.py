211c68d17ba48496f1dc788902fa18642d400a3e
Lavendulaa <kathleen.wheeler@alumni.ubc.ca>
2021-03-14 12:46:06 -0700

    Consolidate temp files

--- data/rawdata/project_functions.py
+++ data/rawdata/project_functions.py
@@ -1,16 +1,20 @@
-import pandas as pd
+# does this need to be here? 
 import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
 
 
-def load_and_process(url_or_path_to_csv_file):
+def load_and_process(path):
 
     # Method Chain 1 (Load data and deal with missing data)
 
     df1 = (
-          pd.read_csv(url_or_path_to_csv_file)
-          .rename(...)
-          .dropna(...)
-          # etc...
+          pd.read_csv(path)
+          .drop_duplicates(subset=['latitude', 'longitude'])
+          .dropna()
+        .reset_index() 
+          
       )
 
     # Method Chain 2 (Create new columns, drop others, and do processing)
@@ -22,4 +26,7 @@ def load_and_process(url_or_path_to_csv_file):
 
     # Make sure to return the latest dataframe
 
-    return df2 
+    return cleaned_data
+
+NA_val = df.isna().sum() 
+
